2b7f73ebee3c77592fff75ce4097fcdfc6814b9c
giorgio <giorgiolink94@hotmail.it>
2022-02-06 16:35:14 +0000

    Update on some of the exercise.

--- questions/m1-D10-ex.js
+++ questions/m1-D10-ex.js
@@ -31,10 +31,71 @@ delete newObj.email
 
     let maxValues  = function() {
 
-        let max = Math.max(onehundred)
-        let min = Math.min(onehundred)
-        let result =  max + min
+        let max = Math.max(...onehundred)
+        let min = Math.min(...onehundred)
+        let result = { max, min}
         return result
     }
 
     console.log(maxValues(onehundred))
+
+    // 1 create the array
+    //2 create 2 array
+    // loop each array until it has 10 random numbers
+
+
+    let randomNumberList = [[],[]]
+    for (i=0, i<=10; i++;){
+        let numbers = (Math.floor(Math.random)*10)
+        randomNumberList[0].push(numbers)
+        randomNumberList[1].push(numbers)
+    }
+    console.log(randomNumberList)                   //????????????????
+
+
+
+
+    let a = [1,2,3,4,5]
+    let b = [1,2,3,4,5,6]
+    let longest = function(a,b){
+        if (a<b)                                    // how to find the lenght of arrays
+        return a;
+        else 
+        return b
+    }   
+
+    console.log(longest())
+    
+        
+    let container = document.getElementById('container')
+    let td = document.getElementsByTagName('th')
+
+
+    
+let print1 = function print1(){
+    for (i=0; i<td.length; i++){
+        console.log(td[i])
+        
+
+    }}
+
+
+
+     let change1 = function change1(){
+        
+        document.getElementById('h1').innerText = '1'
+        
+     }
+
+let addRow = function (){
+    let newTH = document.createElement('th')
+    let parent = document.getElementsByTagName('tr')
+    parent.appendChild(newTH)
+    
+}
+
+
+let nexClass = function(){
+    let x  = document.getElementsByClassName('tr')
+    
+}
